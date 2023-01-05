daa9df6dea2b0c8d0c1aa8eb4cf009f71161af4a
MarianaVer <101337269+MarianaVer@users.noreply.github.com>
2023-01-05 22:37:49 +0200

    Added Git link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by Mariana during SheCodes course and is
+        <a
+          href="https://github.com/MarianaVer/weather-app-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
