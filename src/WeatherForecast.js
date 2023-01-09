a2673618de7de1fbca627edfc7227086a4f947a4
MarianaVer <101337269+MarianaVer@users.noreply.github.com>
2023-01-09 22:06:16 +0200

    Refresh added

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -6,6 +6,10 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
@@ -20,16 +24,16 @@ export default function WeatherForecast(props) {
             <WeatherForecastDay data={forecast[0]} />
           </div>
           <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
+            <WeatherForecastDay data={forecast[1]} />
           </div>
           <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
+            <WeatherForecastDay data={forecast[2]} />
           </div>
           <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
+            <WeatherForecastDay data={forecast[3]} />
           </div>
           <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
+            <WeatherForecastDay data={forecast[4]} />
           </div>
         </div>
       </div>
